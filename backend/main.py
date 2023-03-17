41592c82695c349d7b1d603bafb7fcec53685446
federicores <federicogomezch@gmail.com>
2023-03-17 13:24:07 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,7 @@
 #main.py
 
 from fastapi import FastAPI
-from core.config import settings
-
-app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+app = FastAPI()
 
 @app.get("/")
 def hello_api():
